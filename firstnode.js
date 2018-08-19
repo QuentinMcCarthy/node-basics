c8965c17cf0df4ac7ee9f7a5b336ca1ae4ea81d8
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-08-20 09:27:53 +1200

    Standard input and output

--- firstnode.js
+++ firstnode.js
@@ -1,8 +1,16 @@
-const name = "Quentin";
+// const name = "Quentin";
 
 // console.log("\n Hello, "+name);
-console.log(`\n Hello, ${name}.`);
-console.log(" First node project running! \n");
+// console.log(`\n Hello, ${name}.`);
+// console.log(" First node project running! \n");
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
+
+// process.stdout.write("\nHello from Standard Output.\n\n");
+process.stdout.write("\nWhat is your name?\n\n");
+
+process.stdin.on("data", function(answer){
+	process.stdout.write(`\nHello, ${answer}\n`);
+	process.exit();
+});
