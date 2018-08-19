af484bb5cb88a6652c15cf459d30be44c9c7d540
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-08-20 09:18:23 +1200

    Basic node syntax

---
+++ firstnode.js
@@ -0,0 +1,8 @@
+const name = "Quentin";
+
+// console.log("\n Hello, "+name);
+console.log(`\n Hello, ${name}.`);
+console.log(" First node project running! \n");
+
+console.log(__dirname);
+console.log(__filename);
